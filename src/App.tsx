da6b1f200cc5c1ad76b1d2337b0cf9788edc7815
Sobaken9D <ddrevetnak@gmail.com>
2026-05-16 12:37:42 +0300

    add basic stucture of project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import {cn} from "./lib/utils.ts";
+
+function App() {
+
+  return (
+    <>
+      <h1 className={cn("text-yellow-400", "text-9xl")}>hello</h1>
+    </>
+  )
+}
+
+export default App
